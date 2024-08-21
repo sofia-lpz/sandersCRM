d5cc209f781cb8d648097b0daf707585b837e0ac
sofia-lpz <a01028251@tec.mx>
2024-08-21 13:30:58 -0600

    added preliminary endpoints

--- api/crm.routes.js
+++ api/crm.routes.js
@@ -3,4 +3,22 @@ import * as crmController from './crm.controller.js'
 
 const router = express.Router();
 
+router.get('/donadores', crmController.getDonadores);
+router.get('/donadores/:id', crmController.getDonadorById);
+
+router.post('/donadores', crmController.createDonador);
+router.put('/donadores/:id', crmController.updateDonador);
+
+router.get('/donadores/:id/donaciones', crmController.getDonacionesByDonadorId);
+router.post('/donadores/:id/donaciones', crmController.createDonacion);
+
+router.get('donaciones/:donacionId', crmController.getDonacionById);
+
+router.get('/login', crmController.login);
+
+router.get('/donaciones/:type', crmController.getDonacionesByType);
+
+router.get('/donaciones/fecha/:date', crmController.getDonacionesByDate);
+
+
 export { router }
