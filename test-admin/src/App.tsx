267c669bcfe253f94fb9189947e2b04b3d9f590d
sofia-lpz <a01028251@tec.mx>
2024-10-11 21:21:08 -0600

    added roles

--- test-admin/src/App.tsx
+++ test-admin/src/App.tsx
@@ -4,7 +4,6 @@ import {
   ListGuesser,
   EditGuesser,
   ShowGuesser,
-  usePermissions
 } from "react-admin";
 
 import { Layout } from "./Layout";
@@ -13,21 +12,49 @@ import authProvider from './authProvider';
 
 import { DonacionCreate, DonacionEdit, DonacionList } from "./donaciones";
 import { UsuarioCreate, UsuarioList } from "./usuarios";
-import { DonanteCreate, DonanteShow } from "./donantes";
+import { DonanteCreate } from "./donantes";
 
 import i18nProvider from './i18nProvider';
 import Dashboard from './Dashboard';
 
 export const App = () => (
-  <Admin layout={Layout} 
-  dataProvider={dataProvider} 
-  authProvider={authProvider}
-  i18nProvider={i18nProvider}
+  <Admin
+    layout={Layout}
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    i18nProvider={i18nProvider}
   >
-    <Resource name="usuarios" list={UsuarioList} edit={EditGuesser} create={UsuarioCreate} show={ShowGuesser}/>
-    <Resource name="dashboard" list={Dashboard} options={{ label: 'Dashboard' }} />
-    <Resource name="donaciones" create={DonacionCreate} list={DonacionList} edit={DonacionEdit} show={ShowGuesser}/>
-    <Resource name="donantes" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} create={DonanteCreate}/>
-
+    {permissions => (
+      <>
+        {permissions === 'admin' && (
+          <Resource
+            name="usuarios"
+            list={UsuarioList}
+            edit={EditGuesser}
+            create={UsuarioCreate}
+            show={ShowGuesser}
+          />
+        )}
+        <Resource
+          name="dashboard"
+          list={Dashboard}
+          options={{ label: 'Dashboard' }}
+        />
+        <Resource
+          name="donaciones"
+          create={DonacionCreate}
+          list={DonacionList}
+          edit={DonacionEdit}
+          show={ShowGuesser}
+        />
+        <Resource
+          name="donantes"
+          list={ListGuesser}
+          edit={EditGuesser}
+          show={ShowGuesser}
+          create={DonanteCreate}
+        />
+      </>
+    )}
   </Admin>
 );
