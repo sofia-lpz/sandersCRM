4c27d130f8c951feab8cee626d237205640cb003
sofia-lpz <a01028251@tec.mx>
2024-10-14 10:32:52 -0600

    when small

--- test-admin/src/resources/usuarios.tsx
+++ test-admin/src/resources/usuarios.tsx
@@ -1,4 +1,5 @@
-import { Create, 
+import { 
+    Create, 
     SimpleForm, 
     TextInput, 
     SelectInput,
@@ -10,26 +11,21 @@ import { Create,
     SimpleShowLayout,
     required,
     PasswordInput,
-    FilterList,
-    FilterListItem,
-    CardContentInner,
-    FilterButton,
-    CreateButton,
-    FilterForm,
     RadioButtonGroupInput,
     SearchInput,
+    SimpleList
 } from 'react-admin';
 import { useState } from 'react';
 import PersonIcon from '@mui/icons-material/Person';
-import { Stack } from '@mui/material';
+import { Stack, useMediaQuery, Theme } from '@mui/material';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+
 
 const validateNotEmpty = [required()];
 const validatePasswordsMatch = (value: string, allValues: { password: string }) => {
     return value === allValues.password ? undefined : 'Passwords do not match';
 };
-
-import jsPDF from 'jspdf';
-import autoTable from 'jspdf-autotable';
 
 const UsuariosExporter = (usuarios) => {
     const currentDate = new Date().toLocaleDateString('es-MX');
@@ -59,6 +55,7 @@ const UsuariosExporter = (usuarios) => {
 
     doc.save('usuarios_report.pdf');
 };
+
 const UsuariosFilters = [
     <SearchInput source="q" alwaysOn />,
     <TextInput label="Nombre de Usuario" source="username" />,
@@ -68,15 +65,26 @@ const UsuariosFilters = [
     ]} />,
 ];
 
-export const UsuarioList = () => (
-    <List filters={UsuariosFilters} exporter={UsuariosExporter}>
-        <Datagrid>
-            <TextField label="ID" source="id" />
-            <TextField label="Username" source="username" />
-            <TextField label="Role" source="role" />
-        </Datagrid>
-    </List>
-);
+
+export const UsuarioList = () => {
+    const isSmall = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+    return (
+        <List filters={UsuariosFilters} exporter={UsuariosExporter}>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={(record) => record.username}
+                    secondaryText={(record) => record.role}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField label="ID" source="id" />
+                    <TextField label="Username" source="username" />
+                    <TextField label="Role" source="role" />
+                </Datagrid>
+            )}
+        </List>
+    );
+};
 
 export const UsuarioCreate = () => {
     const [password, setPassword] = useState('');
@@ -107,18 +115,9 @@ export const UsuarioCreate = () => {
     );
 };
 
-const UsuarioFilterList = () => (
-    <CardContentInner>
-        <FilterList label="Role" icon={<PersonIcon />}>
-            <FilterListItem label="Admin" value={{ role: 'admin' }} />
-            <FilterListItem label="User" value={{ role: 'user' }} />
-        </FilterList>
-    </CardContentInner>
-);
-
 export const UsuarioEdit = () => (
     <Edit>
-        <SimpleForm>
+        <SimpleForm toolbar={<MyToolbar />}>
             <TextInput label="Username" source="username" validate={validateNotEmpty}/>
             <SelectInput label="Role" source="role" choices={[
                 { id: 'admin', name: 'Admin' },
