53ebfad9840ee887b4c447fd8c82d7376d2ec67e
sofia-lpz <a01028251@tec.mx>
2024-10-17 08:35:31 -0600

    comments

--- api/app.js
+++ api/app.js
@@ -13,32 +13,25 @@ const PORT = process.env.PORT || 3000;
 const FRONTEND_URL = process.env.FRONTEND_URL || 'https://localhost:5173';
 const MOCK_URL = process.env.MOCK_URL || 'http://localhost:9090';
 
-// Load SSL certificates
 const sslOptions = {
   key: fs.readFileSync('../certificados/server.key'),
   cert: fs.readFileSync('../certificados/server.cert')
 };
 
-// CORS configuration
 const corsOptions = {
-  origin: [FRONTEND_URL, MOCK_URL], // Add your HTTP URL here
+  origin: [FRONTEND_URL, MOCK_URL],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true // If you're using cookies or HTTP authentication
+  credentials: true
 };
 
-// Use CORS middleware with options
 app.use(cors(corsOptions));
 
-// Handle OPTIONS preflight requests
 app.options('*', cors(corsOptions));
 
-// Body parser middleware
 app.use(bodyParser.json());
 
-// Routes
 app.use("/api", router);
 
-// Create the HTTPS server
 https.createServer(sslOptions, app).listen(PORT, () => {
   console.log(`Sanders API listening on HTTPS at port ${PORT}`);
 });
